6b52afea0ba2771e28b5b222b7a7182dd6a05328
eleo-ongaro <eleo.ongaro@gmail.com>
2023-01-05 00:53:27 -0300

    actualizacion3

---
+++ js/js.js
@@ -0,0 +1,19 @@
+
+window.addEventListener('beforeprint', (event) => {
+    console.log('Están por imprimir el CV');
+  })
+  
+//function enviar() {
+  //  var formulario = document.getElementById("miformulario");
+    //var dato = formulario[0];
+ // }
+    //if (dato.value=="enviar") {
+      
+    //  formulario.submit();
+    //  return console.log("solicitud de contacto");
+    //} else {
+    //  alert("Faltan completar campos. Todos son obligatorios. Por favor completar todos los campos y luego pulsar Enviar.  Muchas gracias!");
+    //  return false;
+    //}
+  
+
